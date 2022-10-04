0758328412f920bae04df76a82511abeac6cca64
Martha Staus <mjstaus@gmail.com>
2022-10-04 12:10:40 -0700

    feature: Add link for each blog post

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import { getSortedPostsData } from '../lib/posts';
-import utilStyles from '../styles/utils.module.css';
-
+import Head from "next/head";
+import Link from "next/link";
+import Date from "../components/date";
+import Layout, { siteTitle } from "../components/layout";
+import { getSortedPostsData } from "../lib/posts";
+import utilStyles from "../styles/utils.module.css";
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();  
+  const allPostsData = getSortedPostsData();
   return {
     props: {
       allPostsData,
@@ -13,27 +14,35 @@ export async function getStaticProps() {
   };
 }
 export default function Home({ allPostsData }) {
-  console.log("Posts Data:", allPostsData)
+  console.log("Posts Data:", allPostsData);
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm <b>Martha</b> -  a junior full-stack web developer.</p>
-        <p>This is where I keep my notes on the things I'm learning!</p>
-        <p>Contact me on <a href="https://www.linkedin.com/in/mjstaus/">LinkedIn</a> or <a href="https://twitter.com/mjstau">Twitter</a></p>
+        <p>
+          Hello, I'm <b>Martha</b> - a junior full-stack web developer.
+        </p>
+        <p>I'm learning more every day, and this is where I keep my notes!</p>
+        <p>
+          Contact me on{" "}
+          <a href="https://www.linkedin.com/in/mjstaus/">LinkedIn</a> or{" "}
+          <a href="https://twitter.com/mjstau">Twitter</a>
+        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
